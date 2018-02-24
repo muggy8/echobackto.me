e073f1fe26f84b08b93bd41b6df47dfab81a8ba8
muggy8 <muggy8@gmail.com>
2018-02-24 00:21:02 -0800

    clean up code

--- progressive-web-app-sw.js
+++ progressive-web-app-sw.js
@@ -1,18 +1,4 @@
-// var staticAssets = [
-// 	"/",
-// 	"/js/",
-// 	"/css/styles.css"
-// ]
 var staticAssettContainerName = "static-files";
-
-// self.addEventListener("install", function(ev){
-// 	console.log("install event fired", ev)
-// 	ev.waitUntil(
-// 		caches.open(staticAssettContainerName).then(function(cache){
-// 			return cache.addAll(staticAssets)
-// 		}
-// 	))
-// })
 
 self.addEventListener("fetch", function(ev){
 	// always try to fetch any request from the network and cache the result and only serve the cache if network is down
