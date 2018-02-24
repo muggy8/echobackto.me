4ab804615bfe448228ec72b1fc40db9661cd44f1
muggy8 <muggy8@gmail.com>
2018-02-24 00:20:35 -0800

    finished caching stuff

--- progressive-web-app-sw.js
+++ progressive-web-app-sw.js
@@ -1,32 +1,28 @@
-var staticAssets = [
-	"/",
-	"/js/",
-	"/css/styles.css"
-]
+// var staticAssets = [
+// 	"/",
+// 	"/js/",
+// 	"/css/styles.css"
+// ]
 var staticAssettContainerName = "static-files";
 
-self.addEventListener("install", function(ev){
-	console.log("install event fired", ev)
-	ev.waitUntil(
-		caches.open(staticAssettContainerName).then(function(cache){
-			return cache.addAll(staticAssets)
-		}
-	))
-})
+// self.addEventListener("install", function(ev){
+// 	console.log("install event fired", ev)
+// 	ev.waitUntil(
+// 		caches.open(staticAssettContainerName).then(function(cache){
+// 			return cache.addAll(staticAssets)
+// 		}
+// 	))
+// })
 
 self.addEventListener("fetch", function(ev){
+	// always try to fetch any request from the network and cache the result and only serve the cache if network is down
 	ev.respondWith(
 		caches.open(staticAssettContainerName).then(function(cache){
-			return cache.match(ev.request).then(function(res){
-				if (res){
-					return res;
-				}
-				else {
-					return fetch(ev.request).then(function(fetchRes){
-						cache.put(ev.request, fetchRes.clone())
-						return fetchRes
-					})
-				}
+			return cache.match(ev.request).then(function(cacheRes){
+				return fetch (ev.request).then(function(fetchRes){
+					cache.put(ev.request, fetchRes.clone())
+					return fetchRes || cacheRes;
+				})
 			})
 		})
 	)
