6d55b084e37f0fe411667febe438c0bb91768610
muggy8 <muggy8@gmail.com>
2018-10-14 14:31:39 -0700

    attempted to change the external file path doesn't seem to work tho

--- src/js/recorder.js
+++ src/js/recorder.js
@@ -13,7 +13,7 @@ App.Components.Recorder = (function({div, label, button}){
 			var recorder = context.recorder = new Recorder({
 				encoderApplication: 2048,
 				numberOfChannels: 1,
-				encoderPath: "https://github.com/chris-rudmin/opus-recorder/blob/master/dist/encoderWorker.min.js"
+				encoderPath: "https://unpkg.com/opus-recorder@5.0.0/dist/encoderWorker.min.js"
 			})
 
 			console.log(recorder)
