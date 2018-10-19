f1e0da6a38ccf7d679261dfefd74eacdeebf4714
muggy8 <muggy8@gmail.com>
2018-10-19 02:24:34 -0700

    put the class on the right thing

--- src/js/recordings-list.js
+++ src/js/recordings-list.js
@@ -2,8 +2,8 @@ App.Components.RecordingList = (function({ul, li, audio, a}){
 	return class extends React.Component {
 		render(){
 			// console.log("called", this.state, this.props)
-			return ul(
-				this.props.list.map((recording)=>li({key: recording.id, className: "list"},
+			return ul({className: list}
+				this.props.list.map((recording)=>li({key: recording.id},
 					a({href: recording.audio, download:`${recording.id}.ogg`},
 						`${recording.id}.ogg`
 					),
