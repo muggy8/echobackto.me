38cc052f288198da9e8ae653bf7d584938e7d341
muggy8 <muggy8@gmail.com>
2018-10-19 18:03:17 -0700

    temp typo fixes

--- src/js/recordings-list.js
+++ src/js/recordings-list.js
@@ -2,7 +2,7 @@ App.Components.RecordingList = (function({ul, li, audio, a}){
 	return class extends React.Component {
 		render(){
 			// console.log("called", this.state, this.props)
-			return ul({className: list}
+			return ul({className: "list"},
 				this.props.list.map((recording)=>li({key: recording.id},
 					a({href: recording.audio, download:`${recording.id}.ogg`},
 						`${recording.id}.ogg`
