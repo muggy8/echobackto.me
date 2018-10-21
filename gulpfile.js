cb90c7418639ed62889f8c2dc7f319ce7e786efa
muggy8 <muggy8@gmail.com>
2018-10-21 05:02:58 -0700

    fixed typos and stuff in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,34 +6,34 @@ const htmlmin = require('gulp-htmlmin')
 gulp.task("move", function(){
 	return gulp
 		.src(["src/**/*.js", "src/**/*.css", "src/**/*.html"])
-		.dest("www")
+		.pipe(gulp.dest('www'))
 })
 
-gulp.task("minifyCSS", "move", function(){
+gulp.task("minifyCSS", ["move"], function(){
 	return gulp
 		.src('www/**/*.css')
 		.pipe(cleanCSS())
 		.pipe(gulp.dest('www'))
 })
 
-gulp.task("minifyJS", "minifyCSS", function(){
+gulp.task("minifyJS", ["minifyCSS"], function(){
 	return gulp
 		.src('www/**/*.js')
 	    .pipe(minify())
 	    .pipe(gulp.dest('www'))
 })
 
-gulp.task("minifyHTML", "minifyJS", function(){
+gulp.task("minifyHTML", ["minifyJS"], function(){
 	return gulp
 		.src('www/**/*.html')
 	    .pipe(htmlmin({collapseWhitespace: true}))
 	    .pipe(gulp.dest('www'))
 })
 
-gulp.task("hash", "minifyHTML",  function(){
+gulp.task("hash", ["minifyHTML"],  function(){
 
 })
 
-gulp.task("default", ["hash"] async function(){
+gulp.task("default", ["hash"], async function(){
 
 })
