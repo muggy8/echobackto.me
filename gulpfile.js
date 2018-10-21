02685de5c639e2310efb648cea87a1c41315dba5
muggy8 <muggy8@gmail.com>
2018-10-21 05:00:30 -0700

    added soem dependency for building the app maybe

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,38 @@
+const gulp = require("gulp")
+const minify = require('gulp-minify')
+const cleanCSS = require('gulp-clean-css')
+const htmlmin = require('gulp-htmlmin')
+
 gulp.task("move", function(){
 	return gulp
 		.src(["src/**/*.js", "src/**/*.css", "src/**/*.html"])
 		.dest("www")
 })
 
-gulp.task("minifyCSS", function(){
-
+gulp.task("minifyCSS", "move", function(){
+	return gulp
+		.src('www/**/*.css')
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('www'))
 })
 
-gulp.task("minifyJS", function(){
-
+gulp.task("minifyJS", "minifyCSS", function(){
+	return gulp
+		.src('www/**/*.js')
+	    .pipe(minify())
+	    .pipe(gulp.dest('www'))
 })
 
-gulp.task("minifyHTML", function(){
-
+gulp.task("minifyHTML", "minifyJS", function(){
+	return gulp
+		.src('www/**/*.html')
+	    .pipe(htmlmin({collapseWhitespace: true}))
+	    .pipe(gulp.dest('www'))
 })
 
-gulp.task("hash")
+gulp.task("hash", "minifyHTML",  function(){
+
+})
 
 gulp.task("default", ["hash"] async function(){
 
