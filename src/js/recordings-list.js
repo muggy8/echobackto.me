99ee95f78b50d1957513250cae2056b05648b3d0
muggy8 <muggy8@gmail.com>
2018-10-21 02:45:35 -0700

    app styling is now consistant

--- src/js/recordings-list.js
+++ src/js/recordings-list.js
@@ -2,17 +2,17 @@ App.Components.RecordingList = (function({ul, li, audio, a}){
 	return class extends React.Component {
 		render(){
 			// console.log("called", this.state, this.props)
-			return ul({className: "list"},
-				this.props.list.map((recording)=>li({key: recording.id},
-					a({href: recording.audio, download:`${recording.id}.ogg`},
-						`${recording.id}.ogg`
-					),
+			return ul({className: "list island"},
+				this.props.list.map((recording)=>li({key: recording.id, className: "flex hcenter cell"},
 					audio({
 						autoPlay: true,
 						controls: true,
 						src: recording.audio,
 						onEnded: this.props.playbackComplete,
 					}),
+					a({href: recording.audio, download:`${recording.id}.ogg`, className: "flex vcenter island-h", role: "button"},
+						`Download`
+					),
 				)).reverse()
 			)
 		}
