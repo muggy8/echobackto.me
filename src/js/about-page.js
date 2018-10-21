1cefa06dab25da8195242b9b4c7acc46b4ab1e1a
muggy8 <muggy8@gmail.com>
2018-10-21 02:54:38 -0700

    about is now updated

--- src/js/about-page.js
+++ src/js/about-page.js
@@ -14,6 +14,11 @@ App.Components.AboutPage = (function({div, p, strong, a}){
                     strong("Mobile Workaround 2:"),
                     " To protect users from unwanted data charges, mobile browsers require you to interact with a media source to play it's contents (aka audio and video) so if a video or audio attempts to auto play in a page, it wont be allowed to on mobile and hence protecting you from potential unwanted data charges. Echo Back to Me doesn't load media assets off the net and as a result doesn't have that problem however this still applies to the audio element that is used. to get around this you must be using a stable version of mobile Chrome that's version 47 or later (just update your chrome from the appstore / google play unless you're on a really old device). You must then allow chrome to autoplay media by setting \"disable-gesture-requirement-for-media-playback\" to \"enabled\". it is strongly recommended that you re-disable \"disable-gesture-requirement-for-media-playback\" when you're done using this app for your safety."),
                 p("To enable this work around, visit chrome://flags/#disable-gesture-requirement-for-media-playback and click on the enable button."),
+				p(
+					"This app is built on top of ",
+					a({href: "https://github.com/astroza/chrome_ogg_encoder"}, "astroza's chrome_ogg_encoder"),
+				),
+				p("Feel free to ", a({href: "https://github.com/muggy8/echobackto.me"}, "fork this project on github")),
             )
 		}
 	}
