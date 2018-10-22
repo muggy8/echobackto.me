700e3d77bc2946e3d3da4f001f0dee121750c38d
muggy8 <muggy8@gmail.com>
2018-10-21 21:29:27 -0700

    setup service worker skelton

--- src/js/init.js
+++ src/js/init.js
@@ -2,3 +2,9 @@ ReactDOM.render(
 	React.createElement(App),
 	document.getElementById("root")
 )
+
+if ("serviceWorker" in navigator){
+	window.addEventListener("load", function(){
+		navigator.serviceWorker.register("sw.js")
+	})
+}
